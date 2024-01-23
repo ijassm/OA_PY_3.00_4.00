ac018e708b176f632d0dc1e8599631144557f27e
“ijassm” <ijass.ocean@gmail.com>
2024-01-23 16:20:49 +0530

    -- 23 JAN 2024

---
+++ for_loop.py
@@ -0,0 +1,23 @@
+# range(start, stop, step)
+
+# range(2) start = 0 , stop = 2, step = 1
+# range(1,2) start = 1 , stop = 2, step = 1
+# range(1,2,2) start = 1 , stop = 2, step = 2
+
+# for i in range(1, 11, 3):
+#     print(i)
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+# for i in range(-5, 6, 1):
+#     print(i)
+
+for i in range(1, 26):
+    print("*", end=" ")
+    if i % 5 == 0:
+        print()
+
+# print("hello", end="&")
+# print("world", end="")
+# print("world")
